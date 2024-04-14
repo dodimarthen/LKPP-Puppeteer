95b8d124837fc05fa16201e6cb611e133a71c824
dodimarthen <dodimartin.sit@gmail.com>
2024-04-14 21:24:23 +0700

    modify the variable

--- index.js
+++ index.js
@@ -1,63 +1,49 @@
 const puppeteer = require('puppeteer');
-const { websiteURL } = require('./config.js');
-
+const { websiteURL, username, password } = require('./config.js');
 
 const getQuotes = async () => {
-    // Record the start time
-    const startTime = new Date();
+  // Record the start time
+  const startTime = new Date();
 
-    // Start a Puppeteer session with:
-    // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-    // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-    });
-  
-    // Open a new page
-    console.log("Opening the browser..")
-    const page = await browser.newPage();
-  
-    // On this new page:
-    // - open the "http://quotes.toscrape.com/" website
-    // - wait until the dom content is loaded (HTML is ready)
-    await page.goto(websiteURL, {
-      waitUntil: "domcontentloaded",
-    });
-  
-    // Get page data
-    console.log("scrap the data")
-    const quotes = await page.evaluate(() => {
-      // Fetch the first element with class "quote"
-      // Get the displayed text and returns it
-      const quoteList = document.querySelectorAll(".quote");
-  
-      // Convert the quoteList to an iterable array
-      // For each quote fetch the text and author
-      return Array.from(quoteList).map((quote) => {
-        // Fetch the sub-elements from the previously fetched quote element
-        // Get the displayed text and return it (`.innerText`)
-        const text = quote.querySelector(".text").innerText;
-        const author = quote.querySelector(".author").innerText;
-  
-        return { text, author };
-      });
-    });
-  
-    // Display the quotes
-    console.log("Print the data....")
-    console.log(quotes);
+  // Start a Puppeteer session:
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
 
-    // Calculate the process time
-    const endTime = new Date();
-    const processTime = (endTime - startTime) / 1000;
+  // Open a new page
+  console.log("Opening the browser..")
+  const page = await browser.newPage();
+  await page.goto(websiteURL, {
+    waitUntil: "domcontentloaded",
+  });
+
+  //Login
+  await page.waitForSelector('.input-login[name="username"]');
+  await page.type('.input-login[name="username"]', username);
+  await page.waitForSelector('.input-login[name="password"]');
+  await page.type('.input-login[name="password"]', password);
+  await page.waitForSelector('#btnLoginPenyedia');
+  await page.click('#btnLoginPenyedia');
+  await page.waitForSelector('.modal-header h4');
+  const headerText = await page.evaluate(() => {
+    return document.querySelector('.modal-header h4').textContent;
+  });
+  console.log(headerText);
   
-    // Display the process time
-    console.log("Process time:", processTime, "seconds");
   
-    // Close the browser
-    console.log("Close the browser..")
-    await browser.close();
+  
+  // Close the browser
+  console.log("Close the browser..")
+  await browser.close();
+  
+  // Calculate the process time
+  const endTime = new Date();
+  const processTime = (endTime - startTime) / 1000;
+
+  // Display the process time
+  console.log("Process time:", processTime, "seconds");
+
 };
 
 // Start the scraping
