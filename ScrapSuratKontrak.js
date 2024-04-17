4fa117391d1a03b465c9da8800a3c3dba8cbe0eb
dodimarthen <dodimartin.sit@gmail.com>
2024-04-17 08:40:19 +0700

    create function scrap SK

---
+++ ScrapSuratKontrak.js
@@ -0,0 +1,28 @@
+const ScrapeKontrak = async (page) => {
+    // Click the "Daftar Kontrak" button
+    console.log("Klik 'daftar kontrak' button");
+    await page.click('a[href="/v2/id/purchasing/paket/8929530/daftar-kontrak"]');
+
+    // Wait for the content to load
+    
+    await new Promise(r => setTimeout(r, 1000));
+    await page.waitForSelector('.table');
+
+    // Now you can scrape the data from the table
+    const SuratKontrakItems = await page.evaluate(() => {
+        // Select only the No. Kontrak and Tanggal Kontrak columns
+        const rows = Array.from(document.querySelectorAll('.table tbody tr'));
+
+        return rows.map(row => {
+            const columns = row.querySelectorAll('td');
+            const noKontrak = columns[0].textContent.trim();
+            const tanggalKontrak = columns[1].textContent.trim();
+            return { 'No. Kontrak': noKontrak, 'Tanggal Kontrak': tanggalKontrak };
+        });
+    });
+
+    // Return the extracted data
+    return SuratKontrakItems;
+};
+
+module.exports = ScrapeKontrak;
