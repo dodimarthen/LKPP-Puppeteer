d94766f9ef9ff9c0d5cbdf5b21bc356f44f1fca1
dodimarthen <dodimartin.sit@gmail.com>
2024-05-02 11:13:59 +0700

    add condition

--- ScrapRiwayatNegosiasi.js
+++ ScrapRiwayatNegosiasi.js
@@ -1,13 +1,12 @@
 const scrapNego = async (page, href) => {
     try {
         //Debugging process
-        console.log('Scraping data riwayat negosiasi')
         // Navigate to the specific URL containing the table
         await page.goto(`https://e-katalog.lkpp.go.id${href}`, {
             waitUntil: "domcontentloaded",
         });
         // adding time.sleep
-        await new Promise(resolve => setTimeout(resolve, 17000));
+        await new Promise(resolve => setTimeout(resolve, 15000));
 
         // Extract data from the table
         const data = await page.evaluate(() => {
@@ -18,6 +17,11 @@ const scrapNego = async (page, href) => {
             });
         });
         
+        // Check if there are any history items or cells
+        if (data.length === 0 || data.every(row => row.length === 0)) {
+            return "no history of negotiation";
+        }
+
         // Find the index of the last occurrence of any revision
         let lastIndex = -1;
         for (let i = data.length - 1; i >= 0; i--) {
