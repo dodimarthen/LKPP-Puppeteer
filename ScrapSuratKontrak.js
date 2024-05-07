7968f35bffc7f5941bdb651bbe5abacc84cc37e2
dodimarthen <dodimartin.sit@gmail.com>
2024-05-07 15:44:03 +0700

    modify catch error

--- ScrapSuratKontrak.js
+++ ScrapSuratKontrak.js
@@ -1,29 +1,33 @@
 async function scrapeKontrak(page, href) {
-    await page.goto(`https://e-katalog.lkpp.go.id${href}`, {
-        waitUntil: "domcontentloaded",
-    });
-    await page.waitForSelector('.table');
-    const contractDetails = await page.evaluate(() => {
-        const table = document.querySelector('.table');
+    try {
+        await page.goto(`https://e-katalog.lkpp.go.id${href}`, {
+            waitUntil: "domcontentloaded",
+        });
+        await page.waitForSelector('.table');
+        const contractDetails = await page.evaluate(() => {
+            const table = document.querySelector('.table');
 
-        if (!table) {
-            throw new Error('Table not found');
-        }
+            if (!table) {
+                throw new Error('Table not found');
+            }
 
-        // Check if "No data found" message is present in the table
-        const noDataElement = table.querySelector('tbody tr td[colspan="6"] center');
-        if (noDataElement && noDataElement.textContent.trim() === "No data found") {
-            return {'No.Kontrak': 'Null', 'Tanggal Kontrak': 'Null'};
-        }
+            // Check if "No data found" message is present in the table
+            const noDataElement = table.querySelector('tbody tr td[colspan="6"] center');
+            if (noDataElement && noDataElement.textContent.trim() === "No data found") {
+                return {'No.Kontrak': 'Null', 'Tanggal Kontrak': 'Null'};
+            }
 
-        const contractNumber = table.querySelector('tbody tr td:nth-child(1)').textContent.trim();
-        const contractDate = table.querySelector('tbody tr td:nth-child(2)').textContent.trim();
+            const contractNumber = table.querySelector('tbody tr td:nth-child(1)').textContent.trim();
+            const contractDate = table.querySelector('tbody tr td:nth-child(2)').textContent.trim();
 
-        return {'No.Kontrak': contractNumber, 'Tanggal Kontrak': contractDate};
-    });
+            return {'No.Kontrak': contractNumber, 'Tanggal Kontrak': contractDate};
+        });
 
-    return contractDetails;
-    await new Promise(resolve => setTimeout(resolve, 2000));
+        return contractDetails;
+    } catch (error) {
+        console.error(`An error occurred: ${error}`);
+        return {'No.Kontrak': 'Error', 'Tanggal Kontrak': 'Error'};
+    }
 }
 
 module.exports = scrapeKontrak;
