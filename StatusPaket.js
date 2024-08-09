14a053af93b2c1fa85981bab2565900c656d6207
dodimarthen <dodimartin.sit@gmail.com>
2024-08-09 14:06:49 +0700

    modify output return

--- StatusPaket.js
+++ StatusPaket.js
@@ -1,13 +1,11 @@
-import { logInformasiUtamaPemesanPPK } from "./MainPage.js";
-import { logInformasiDaftarKontrak } from "./packageKontrak.js";
 import { TableNego } from "./HargaNegosiasi.js";
 
 export async function logTableLinks(page) {
   try {
     const data = await page.evaluate(() => {
       const rows = document.querySelectorAll("tbody tr");
-      return Array.prototype.map
-        .call(rows, (row) => {
+      return Array.from(rows)
+        .map((row) => {
           const anchor = row.querySelector("a[href*='detail']");
           const href = anchor ? anchor.href : null;
           const td11 = row.cells[12] ? row.cells[12].innerText.trim() : null;
@@ -18,18 +16,25 @@ export async function logTableLinks(page) {
           ) {
             return { href, td11 };
           }
-
           return null;
         })
         .filter((item) => item !== null);
     });
+
+    data.forEach((item) => {
+      console.log(item.href);
+    });
+    const results = [];
+
     for (const item of data) {
-      console.log(item);
       await page.goto(item.href);
-      // await logInformasiUtamaPemesanPPK(page);
-      // await logInformasiDaftarKontrak(page);
-      await TableNego(page);
+      const tableNegoResult = await TableNego(page);
+      const pairedResult = { ...item, tableNegoResult };
+      results.push(pairedResult);
+      console.log(pairedResult);
     }
+
+    return results;
   } catch (error) {
     console.error(`Error fetching data: ${error.message}`);
   }
